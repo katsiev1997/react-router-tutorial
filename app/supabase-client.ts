07822af182442512180feb2dbe360121b030bc87
katsiev1997 <katsiev1997@gmail.com>
2025-04-29 23:44:33 +0300

    fix supabase-client

--- app/supabase-client.ts
+++ app/supabase-client.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ynfqrbvmjobreiaysudv.supabase.co";
-const SUPABASE_ANON_KEY =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InluZnFyYnZtam9icmVpYXlzdWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NTUxMTUsImV4cCI6MjA2MTUzMTExNX0.1p0XEVZ6qnlIFjSTe8WU_gX0tTknoB35zqnU09OXAXY";
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
